46d5082c9d433217ea13d3fdd10dbf022308b257
Taro Aoki <aizu.s1230022@gmail.com>
2016-06-19 22:28:49 +0900

    [Add] Check seats and date tests

---
+++ tests/check-target-seats.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var fs     = require("fs");
+
+var casper = require("casper").create({
+  verbose : true,
+  logLevel : "debug"
+});
+
+var URL = "http://localhost:8888"
+
+var wishSeats = JSON.parse(fs.read("config.json"))["wish-seats"];
+// var wishSeats = [["F1", "F2"], ["B6", "B5"]];
+
+var t = Date.now();
+
+casper.start(URL, function() {
+  this.echo("Title: " + this.getTitle());
+  this.echo("Loading time: " + (Date.now() - t) + "ms");
+});
+
+casper.thenEvaluate(function(seats) {
+  firstLoop : for (var i = 0; i < seats.length; i++) {
+    for (var j = 0; j < seats[i].length; j++) {
+      if (document.querySelector("#" + seats[i][j] + "_base").className == "reserved") {
+        continue firstLoop;
+      }
+    }
+
+    // All seat are vacancy.
+    for (var j = 0; j < seats[i].length; j++) {
+      document.querySelector("#" + seats[i][j]).click();
+    }
+    break;
+  }
+
+}, wishSeats);
+
+casper.then(function() {
+  this.capture("./img/check-target-seats.png");
+});
+
+casper.run();
